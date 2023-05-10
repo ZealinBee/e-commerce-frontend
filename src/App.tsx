cfde5746293b9c604ab65890de228ab76e3ae226
Zhiyuan Liu <thenukezealot@gmail.com>
2023-05-10 13:38:38 +0300

    route and interface initial set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import ProfilePage from "./pages/ProfilePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products/:id" element={<ProductPage></ProductPage>} />
+          <Route path="/users/:id" element={<ProductPage></ProductPage>} />
+          <Route path="*" element={<NotFoundPage></NotFoundPage>} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
-export default App
+export default App;
