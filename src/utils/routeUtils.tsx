aa68ce32b37f30a64e72d846fb093b1034ba5aff
Zhiyuan Liu <thenukezealot@gmail.com>
2023-10-25 12:03:43 +0300

    removed unnessecary utils

--- src/utils/routeUtils.tsx
+++ src/utils/routeUtils.tsx
@@ -9,48 +9,54 @@ import Modification from "../pages/Modification";
 import SignUp from "../pages/SignUp";
 import Login from "../pages/Login";
 
-
-
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <HomePage />,
-      errorElement: <NotFoundPage />,
-    },
-    {
-      path: "/cart",
-      element: <Cart />,
-      errorElement: <NotFoundPage />,
-    },
-    {
-      path: "/products/:id",
-      element: <ProductPage />,
-      errorElement: <NotFoundPage />,
-    },
-    {
-      path: "/users/:id",
-      element: <ProfilePage />,
-      errorElement: <NotFoundPage />,
-    },
-    {
-      path: "/modification",
-      element: <Modification />,
-      errorElement: <NotFoundPage />,
-    },
-    {
-      path: "/signup",
-      element: <SignUp />,
-      errorElement: <NotFoundPage />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-      errorElement: <NotFoundPage />,
-    },
-    {
-      path: "/profile",
-      element: <ProfilePage />,
-      errorElement: <NotFoundPage />,
-    },
-  ]);
-  
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/products/:id",
+    element: <ProductPage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/users/:id",
+    element: <ProfilePage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/modification",
+    element: <Modification />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/profile",
+    element: <ProfilePage />,
+    errorElement: <NotFoundPage />,
+  },
+]);
+export {};
+
+function routeUtils() {
+  return (
+    <div>routeUtils</div>
+  )
+}
+
+export default routeUtils
